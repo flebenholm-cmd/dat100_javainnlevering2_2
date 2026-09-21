4cf40eeed83eed7c5742c7d117b6738581709f5d
flebenholm-cmd <flebenholm@gmail.com>
2026-09-21 14:08:25 +0200

    første forsøk på summer metoden

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -29,7 +29,11 @@ public class Tabeller {
 	public static int summer(int[] tabell) {
 
 		// TODO
-		throw new UnsupportedOperationException("Metoden summer ikke implementert");
+		int sum = 0;
+		for (int i = 0; i<tabell.length; i++) {
+			sum += tabell[i];
+		}
+		return sum;
 	}
 
 	// d)
