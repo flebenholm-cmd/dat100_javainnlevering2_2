c3dc4faa1c8bb78a0451e460fab97e6902518950
BrageHVL <brtog3393@hvl.no>
2026-09-22 16:07:29 +0200

    Implementerer Matriser.skaler

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -38,10 +38,17 @@ public class Matriser {
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
+
+		// Ny utgangsmatrise med lik størrelse som inngangsmatrisen
+		int[][] returMat = new int[matrise.length][matrise[0].length];
+
+		for (int i = 0; i < matrise.length; i++) {
+			for (int j = 0; j < matrise[i].length; j++) {
+
+				returMat[i][j] = matrise[i][j] * tall;
+			}
+		}
+	return returMat;
 	}
 
 	// d)
